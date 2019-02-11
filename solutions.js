d9e071692bf37946c5b2cee542a220cc425c89e2
Dwight Bemisderfer <dwight.bemisderfer@gmail.com>
2019-02-11 11:49:00 -0600

    completion of solutions.js mock assessment

--- solutions.js
+++ solutions.js
@@ -20,3 +20,84 @@
  * and
  * concat
  */
+
+function concat(inputConcat_x, inputConcat_y) {
+    var inputConcat_x = inputConcat_x.toString();
+    var inputConcat_y = inputConcat_y.toString();
+    return inputConcat_x + inputConcat_y;
+}
+
+console.log(concat("true", "true"));
+console.log(concat("hello", "world"));
+console.log(concat(4, 2));
+
+
+function isEqual(isEqual_x, isEqual_y) {
+    var isEqual_x = +isEqual_x;
+    var isEqual_y = +isEqual_y;
+    return isEqual_x === isEqual_y;
+}
+
+isEqual("java", "javascript");
+isEqual("3.6", 3.6);
+
+
+function or(or_x, or_y) {
+    return or_x || or_y;
+}
+
+or();
+
+
+function and(and_x, and_y) {
+    return and_x && and_y;
+}
+
+and();
+
+
+function isIdentical(isIdentical_x, isIdentical_y){
+    return isIdentical_x === isIdentical_y;
+}
+
+isIdentical();
+
+
+function addOne(addOne_x) {
+    var addOne_x = +addOne_x;
+    return addOne_x += 1;
+}
+
+console.log("addOne: " + addOne("0"));
+
+
+function isEven(isEven_x) {
+    if (isEven_x === false) {
+        return isEven_x;
+    }
+    return isEven_x % 2 === 0;
+}
+
+console.log("isEven:" + isEven(false));
+
+
+function not(not_x) {
+    return !not_x === true;
+}
+
+console.log("Not true: " + not(true));
+console.log("Not false: " + not(false));
+
+
+function isFalse(isFalse_x) {
+    return isFalse_x === false;
+}
+
+isFalse("true");
+
+
+function isTrue(isTrue_x) {
+    return isTrue_x === true;
+}
+
+isTrue("6");
