e7e2bf1b3ad3fe7bdbd03dc6c395a18e9b6b422e
Marcus Aguirre <marcus.a.aguirre@gmail.com>
2019-02-11 11:14:28 -0600

    finished work on mock assessment

--- solutions.js
+++ solutions.js
@@ -20,3 +20,65 @@
  * and
  * concat
  */
+
+
+function isTrue (input) {
+    return input === true;
+
+}
+
+function isFalse(input) {
+    return input === false;
+
+}
+
+function not(input) {
+    return input == false || input == null || input == undefined || input == "" ;
+
+
+}
+
+function addOne(input) {
+    var new_input = +input + 1;
+    return new_input;
+
+}
+
+
+function isEven(input){
+    return input % 2 === 0;
+
+}
+
+
+function isIdentical(one, two) {
+    return one === two;
+
+
+}
+
+function isEqual(one, two) {
+    return one == two;
+
+
+}
+
+
+function or(one, two){
+    return one;
+
+
+}
+
+function and(one, two) {
+    return two;
+
+
+}
+
+function concat(one, two) {
+
+    return (one) + (two);
+
+
+}
