bfbb0cacd00efe680b33ed1332a78f5592b555a5
Cory Musick <cory.musick37@gmail.com>
2019-02-11 11:05:01 -0600

    completed mock assesment

--- solutions.js
+++ solutions.js
@@ -3,6 +3,8 @@
 /**
  * Write your solutions here.
  *
+ *
+ *
  * **Note**: While normally it is good practice to wrap your javascript in an
  * immediately invoked function expression (iife), you should _not_ do that
  * here. If you do, the automated tests will not be able to see your functions.
@@ -20,3 +22,43 @@
  * and
  * concat
  */
+
+function isTrue(input){
+    return input === true;
+}
+
+function isFalse(input){
+    return input === false;
+}
+
+function not(input){
+    return !input;
+}
+
+function addOne(input){
+    return +input + 1;
+}
+
+function isEven(input){
+    return input % 2 === 0;
+}
+
+function isIdentical(input1, input2){
+    return input1 === input2;
+}
+
+function isEqual(input1, input2){
+    return input1 == input2;
+}
+
+function or(input1, input2){
+    return input1 || input2;
+}
+
+function and(input1, input2){
+    return input1 && input2;
+}
+
+function concat(input1, input2){
+    return input1.toString() + input2.toString();
+}
