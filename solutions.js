8912779ad3a3a6524a9e005179809478b645b740
laroblesaguilar <laroblesaguilar@gmail.com>
2019-02-11 11:04:06 -0600

    completed mock assessment

--- solutions.js
+++ solutions.js
@@ -9,14 +9,54 @@
  * If you don't know what an iffe is, don't worry about it :)
  *
  * This file is for defining the following functions. See README.md for more detail.
- * isTrue
- * isFalse
- * not
- * addOne
- * isEven
- * isIdentical
+ * isTrue done
+ * isFalse done
+ * not done
+ * addOne done
+ * isEven done
+ * isIdentical done
  * isEqual
- * or
+ * or done
  * and
  * concat
  */
+
+function isTrue(input){
+    return input == true;
+}
+
+function isFalse(input){
+    return input == false;
+}
+
+function not(input){
+    return !input == true;
+}
+
+function addOne(num){
+    return (Number(num) + 1);
+}
+
+function isEven(num){
+    return (num % 2 === 0);
+}
+
+function or(inputA, inputB){
+    return (inputA || inputB);
+}
+
+function isIdentical(inputA, inputB){
+    return (inputA === inputB);
+}
+
+function isEqual(inputA, inputB){
+    return (inputA == inputB);
+}
+
+function and(inputA, inputB){
+    return (inputA && inputB);
+}
+
+function concat(inputA, inputB){
+    return (inputA.toString() + inputB);
+}
