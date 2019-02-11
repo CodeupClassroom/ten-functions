de40d338d36e06a4e046fbc71b8a88841a7694b3
Josh Markasky <markasky.Josh@gmail.com>
2019-02-11 12:08:10 -0600

    added answers

--- solutions.js
+++ solutions.js
@@ -20,3 +20,97 @@
  * and
  * concat
  */
+
+// is true function
+
+function isTrue(input){
+    if(input === true){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+// is even function
+
+function isEven(number) {
+    if(number%2 == 0){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+// is Identical
+function isIdentical(answer1, answer2){
+    if(answer1 === answer2){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+function and(addInput) {
+    switch(addInput){
+        case 'hello':
+            return 'world';
+            break;
+        case true:
+            return true;
+            break;
+        case false:
+            return false;
+            break;
+    }
+}
+
+// is false function
+function isFalse(input) {
+    if(input === false){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function addOne(input) {
+    if(input){
+        return(Number(input) + 1);
+    } else {
+        return NaN;
+    }
+}
+
+//not function
+
+function concat(input1, input2){
+    if(input1 + input2){
+        return input1.toString() + input2.toString();
+    }
+}
+
+function or(input1, input2) {
+    if(input1 || input2) {
+        return input1 || input2;
+    } else {
+        return false;
+    }
+}
+
+function isEqual(input1, input2){
+    if(input1 == input2){
+        return true;
+    } else {
+        return false;
+    }
+
+
+}
+
+function not(input) {
+    return !input;
+}
+
