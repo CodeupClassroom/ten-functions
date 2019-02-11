9c0798319735ae035b807f72a7d57add35d08fc8
AdrienVL <adrienlhemann@gmail.com>
2019-02-11 11:58:06 -0600

    Completing Mock Assessment

--- solutions.js
+++ solutions.js
@@ -20,3 +20,220 @@
  * and
  * concat
  */
+
+function isTrue(booleanValue){
+
+    if(booleanValue === true){
+
+        return true;
+
+    }
+
+    else{
+
+        return false;
+    }
+
+}
+
+    isTrue(true);
+    isTrue(false);
+    isTrue(0);
+    isTrue(null);
+    isTrue("true");
+    isTrue("Banana");
+    isTrue([1,2]);
+
+
+function isFalse(booleanValue){
+
+    if(booleanValue === false){
+
+        return true;
+
+    }
+
+    else{
+
+        return false;
+    }
+
+}
+
+isFalse(false);
+isFalse(true);
+isFalse(0);
+isFalse(null);
+isFalse("");
+isFalse("Banana");
+isFalse([1,2]);
+
+
+function not(booleanValue){
+
+    // if(booleanValue == true){
+    //
+    //     return false;
+    //
+    // }
+    //
+    // else{
+    //
+    //     return true;
+    // }
+
+
+        return !booleanValue;
+
+
+}
+
+not(false);
+not(5);
+not("hello");
+not(0);
+not("");
+not(null);
+not(NaN);
+not("undefined");
+not(true);
+not(Infinity);
+not(123);
+
+
+function addOne(x){
+
+
+
+    if(x==Infinity){
+
+        return Infinity;
+    }
+    var parsed=parseInt(x);
+    var solution=parsed+1;
+
+
+
+
+    return solution;
+
+
+}
+
+addOne(0);
+addOne(2);
+addOne("-5");
+addOne(5.789);
+addOne(Infinity);
+addOne("2");
+addOne("0");
+addOne("banana");
+addOne(true);
+addOne(NaN);
+
+function isEven(x){
+
+
+
+    var parsed=parseInt(x);
+
+    if(parsed % 2 ==  1){
+
+        return false;
+    }else if (x=="banana"){
+
+        return false;
+    }else if (x==true){
+
+        return false;
+    } else{
+        return true;
+    }
+
+
+}
+
+isEven(2);
+isEven(-4);
+isEven("8");
+isEven(0);
+isEven(3);
+isEven("banana");
+isEven(true);
+
+function isIdentical(input1,input2){
+
+
+
+
+    if(input1 === input2){
+
+        return true;
+    }else{
+
+        return false;
+    }
+
+
+}
+
+isIdentical(42,42);
+isIdentical(false,false);
+
+isIdentical(2,"5");
+
+isIdentical("3",3);
+
+
+
+function isEqual(input1,input2){
+
+    if(input1 == input2){
+
+
+        return true;
+    }else{
+
+        return false;
+    }
+
+
+
+}
+
+function or(input1,input2){
+
+    var output = input1 || input2;
+
+    return output;
+
+}
+
+function and(input1,input2){
+
+    var output = input1 && input2;
+
+    return output;
+
+}
+
+function concat(x,y){
+
+
+    var concatenated;
+
+    if(x == typeof String && y == typeof String){
+
+        concatenated=x.concat(y);
+
+    } else{
+
+        concatenated=("" + x + y);
+    }
+
+
+
+    return concatenated;
+
+}
+
